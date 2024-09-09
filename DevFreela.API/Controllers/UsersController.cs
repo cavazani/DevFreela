a5d38f8f399740b09101943e6b0afb3511dac9ff
cavazani <gabriel_cavazani@hotmail.com>
2024-09-09 15:54:48 -0300

    Exemplo recebendo arquivos na API

--- DevFreela.API/Controllers/UsersController.cs
+++ DevFreela.API/Controllers/UsersController.cs
@@ -14,5 +14,15 @@ namespace DevFreela.API.Controllers {
             return Ok();
         }
 
+        [HttpPut("{id}/profile-picture")] 
+        public IActionResult PostProfilePicture(IFormFile file)
+        {
+            var description = $"File: {file.FileName}, Size: {file.Length}";
+
+            //Processar a Imagem
+
+            return Ok(description);
+        }
+
     }
 }
