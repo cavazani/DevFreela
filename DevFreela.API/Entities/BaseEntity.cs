5ed129046fc73d8e075b0b70ca3d964db37f1956
cavazani <gabriel_cavazani@hotmail.com>
2024-09-12 23:51:30 -0300

    Adicionado pacotes do EntityFramework

---
+++ DevFreela.API/Entities/BaseEntity.cs
@@ -0,0 +1,20 @@
+﻿namespace DevFreela.API.Entities {
+    public abstract class BaseEntity 
+    {
+        protected BaseEntity() 
+        {
+            CreatedAt = DateTime.Now;
+            IsDeleted = false;
+        }
+
+        public int Id { get; set; }
+        public DateTime CreatedAt { get; set; }
+        public bool IsDeleted { get; set; }
+
+
+        public void SetAsDeleted()
+        {
+            IsDeleted = true;
+        }
+    }
+}
--- DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
+++ DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c8d097e388aed621072d4a54653261abaf204243")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5b60797a0f215d22852611966e9ef107c6f41a03")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
