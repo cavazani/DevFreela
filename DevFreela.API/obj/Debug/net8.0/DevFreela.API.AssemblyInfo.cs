390ad6c47512f23c42e77b666c29d2361cc673ac
cavazani <gabriel_cavazani@hotmail.com>
2024-09-22 23:29:39 -0300

    Primeira migration

--- DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
+++ DevFreela.API/obj/Debug/net8.0/DevFreela.API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b7a3cf6b538549f7bf5e08888b6f0f7059e19a57")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f8a38a930a11a81fc68dc89910ecfd984de3749a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DevFreela.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
