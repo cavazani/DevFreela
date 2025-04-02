149cf6846afdbcbbb626f3ee8eb2f2ce4021cf92
cavazani <gabriel_cavazani@hotmail.com>
2025-04-02 00:03:22 -0300

    Inclusão do projeto de teste unitario e criando primeiro teste

---
+++ DevFreela.UnitTests/Core/ProjectTests.cs
@@ -0,0 +1,30 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.UnitTests.Core 
+{
+    public class ProjectTests 
+    {
+        [Fact]
+        public void ProjectIsCreated_Strat_Success() 
+        {
+            // Arrange
+            var project = new Project("Projeto A", "Descriação do Projeto", 1, 2, 10000);
+
+            // Act
+            project.Start();
+
+            // Assert
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+
+            Assert.True(project.Status == ProjectStatusEnum.InProgress);
+            Assert.False(project.StartedAt is null);
+        }
+    }
+}
